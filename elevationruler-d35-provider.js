e40f1e722a39086bbb0f806ece90f1dfd3c4290a
daedalus11069 <daedalus969@gmail.com>
2025-03-06 08:12:25 -0800

    Initial commit

---
+++ elevationruler-d35-provider.js
@@ -0,0 +1,42 @@
+Hooks.once("setup", () => {
+  const walk = {
+    name: "Walk",
+    color: Color.from(0x00ff00),
+    multiplier: 1
+  };
+  const dash = {
+    name: "Dash",
+    color: Color.from(0xffff00),
+    multiplier: 2
+  };
+  const run = {
+    name: "Run",
+    color: Color.from(0xff8000),
+    multiplier: 4
+  };
+  const max = {
+    name: "Maximum",
+    color: Color.from(0xff0000),
+    multiplier: Infinity
+  };
+
+  CONFIG.elevationruler.SPEED.CATEGORIES.splice(
+    0,
+    CONFIG.elevationruler.SPEED.CATEGORIES.length,
+    ...[walk, dash, run, max]
+  );
+
+  CONFIG.elevationruler.MOVEMENT_TYPES.SWIM = 3; // Increment by 1 from the highest-valued movement type
+
+  // This label is from Font Awesome
+  CONFIG.elevationruler.MOVEMENT_BUTTONS[
+    CONFIG.elevationruler.MOVEMENT_TYPES.SWIM
+  ] = "person-swimming";
+  CONFIG.elevationruler.SPEED.ATTRIBUTES.SWIM =
+    "actor.system.attributes.speed.swim.total"; // dnd35e
+
+  CONFIG.elevationruler.pathfindingCheckTerrains = true;
+
+  CONFIG.elevationruler.SPEED.useFontAwesome = true;
+  CONFIG.elevationruler.SPEED.terrainSymbol = "\uf071";
+});
