0f114e441a6b6b7c6c286b76016dde7d88f5d250
248th-Desolation-Community <163466430+248th-Desolation-Community@users.noreply.github.com>
2025-03-25 13:13:12 -0300

    Updated movement to working version?

--- elevationruler-dw-provider.js
+++ elevationruler-dw-provider.js
@@ -6,8 +6,13 @@ Hooks.once("setup", () => {
   };
   const dash = {
     name: "Dash",
-    color: Color.from(0xff8000),
+    color: Color.from(0xffff00),
     multiplier: 2
+  };
+  const run = {
+    name: "Run",
+    color: Color.from(0xff8000),
+    multiplier: 4
   };
   const max = {
     name: "Maximum",
@@ -18,20 +23,17 @@ Hooks.once("setup", () => {
   CONFIG.elevationruler.SPEED.CATEGORIES.splice(
     0,
     CONFIG.elevationruler.SPEED.CATEGORIES.length,
-    ...[walk, dash, max]
+    ...[walk, dash, run, max]
   );
 
   CONFIG.elevationruler.MOVEMENT_TYPES.SWIM = 3; // Increment by 1 from the highest-valued movement type
-  
-  
+
   // This label is from Font Awesome
   CONFIG.elevationruler.MOVEMENT_BUTTONS[
     CONFIG.elevationruler.MOVEMENT_TYPES.SWIM
   ] = "person-swimming";
-  CONFIG.elevationruler.SPEED.ATTRIBUTES.WALK =
-    "actor.system.props.walkingSpeed";
-    CONFIG.elevationruler.SPEED.ATTRIBUTES.WALK =
-    "actor.system.props.SwimmingSpeed";
+  CONFIG.elevationruler.SPEED.ATTRIBUTES.SWIM =
+    "actor.system.props.walkingSpeed"; 
 
   CONFIG.elevationruler.pathfindingCheckTerrains = true;
 
